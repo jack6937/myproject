4a3674033d8b1b4e85ae34ea1fd6ad62f3014aec
leesk <jack6937@naver.com>
2017-11-30 11:47:15 +0900

    join edit function plus

--- ex01/routes/questions.js
+++ ex01/routes/questions.js
@@ -2,8 +2,10 @@ const express = require('express');
 const Question = require('../models/question');
 const Answer = require('../models/answer'); 
 const catchErrors = require('../lib/async-error');
-
+const Join = require('../models/join');
 const router = express.Router();
+
+
 
 // 동일한 코드가 users.js에도 있습니다. 이것은 나중에 수정합시다.
 function needAuth(req, res, next) {
@@ -15,6 +17,55 @@ function needAuth(req, res, next) {
   }
 }
 
+function validateForm(form, options) {
+  var title = form.title || "";
+  var content = form.content || "";
+  var location = form.location || "";
+  var start = form.start || "";
+  var finish = form.finish || "";
+  var host = form.host || "";
+  var maximum = form.maximum || "";
+  var hostcontent = form.hostcontent || "";
+  title = title.trim();
+  content = content.trim();
+  location = location.trim();
+
+  host = host.trim();
+  hostcontent = hostcontent.trim();
+
+  if (!title) {
+    return '이벤트 제목은 필수입력사항입니다.';
+  }
+
+  if (!content) {
+    return '이벤트 내용은 필수입력사항입니다.';
+  }
+  
+  if (!maximum) {
+    return '참가인원은 필수입력사항입니다.';
+  }
+
+  if (!location) {
+    return '장소는 필수입력사항입니다..';
+  }
+
+  if (!start) {
+    return '시작시간은 필수입력사항입니다.';
+  }
+  
+  if (!finish) {
+    return '종료시간은 필수입력사항입니다.';
+  }
+
+  if (!host) {
+    return '등록조직 이름은 필수입력사항입니다.';
+  }
+
+  if (!hostcontent) {
+    return '등록조직 설명은 필수입력사항입니다.';
+  }
+  return null;
+}
 /* GET questions listing. */
 router.get('/', catchErrors(async (req, res, next) => {
   const page = parseInt(req.query.page) || 1;
@@ -56,9 +107,13 @@ router.get('/:id', catchErrors(async (req, res, next) => {
 
 router.put('/:id', catchErrors(async (req, res, next) => {
   const question = await Question.findById(req.params.id);
-
+  var err = validateForm(req.body);
   if (!question) {
     req.flash('danger', 'Not exist question');
+    return res.redirect('back');
+  }
+  if (err) {
+    req.flash('danger', err);
     return res.redirect('back');
   }
   question.title = req.body.title;
@@ -69,7 +124,9 @@ router.put('/:id', catchErrors(async (req, res, next) => {
   question.start = req.body.start;
   question.finish = req.body.finish;
   question.host = req.body.host;
-  question.hostcontent = req.body.hostcontent
+  question.maximum =req.body.maximum;
+  question.price =req.body.price;
+  question.hostcontent = req.body.hostcontent;
   question.tags = req.body.tags.split(" ").map(e => e.trim());
 
   await question.save();
@@ -83,8 +140,14 @@ router.delete('/:id', needAuth, catchErrors(async (req, res, next) => {
   res.redirect('/questions');
 }));
 
+
 router.post('/', needAuth, catchErrors(async (req, res, next) => {
   const user = req.user;
+  var err = validateForm(req.body);
+  if (err) {
+    req.flash('danger', err);
+    return res.redirect('back');
+  }
   var question = new Question({
     title: req.body.title,
     author: user._id,
@@ -97,6 +160,7 @@ router.post('/', needAuth, catchErrors(async (req, res, next) => {
     host: req.body.host,
     price: req.body.price,
     hostcontent: req.body.hostcontent,
+    maximum: req.body.maximum,
     tags: req.body.tags.split(" ").map(e => e.trim()),
   });
   await question.save();
@@ -126,6 +190,34 @@ router.post('/:id/answers', needAuth, catchErrors(async (req, res, next) => {
   res.redirect(`/questions/${req.params.id}`);
 }));
 
+//참여신청
+router.post('/:id/join', needAuth, catchErrors(async (req, res, next) => {
+  const user = req.user;
+  const question = await Question.findById(req.params.id);
+
+  if (!question) {
+    req.flash('danger', 'Not exist question');
+    return res.redirect('back');
+  }
+  var finduser = await Join.findOne({author: req.user._id, question: question._id});//??
+  if (!finduser){//추가
+    if(question.maximum>question.numParticipate){
+      var join = new Join({
+        author: user._id,
+        question: question._id
+      }); 
+      await join.save();
+      question.numParticipate++;
+      req.flash('success', '참가 신청 완료');
+    }else{
+      req.flash('danger', '참가 인원 초과');
+    }
+    await question.save();
+  }else{
+      req.flash('danger', '이미 신청되었습니다');
+  }
+  res.redirect('back');
+}));
 
 
 module.exports = router;
