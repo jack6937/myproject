9c27b44fc3ea77f6b85e88da6d63a0b9e9a9693b
leesk <jack6937@naver.com>
2017-12-08 03:35:06 +0900

    kakao 2

--- ex01/models/user.js
+++ ex01/models/user.js
@@ -8,6 +8,7 @@ var schema = new Schema({
   email: {type: String, required: true, index: true, unique: true, trim: true},
   password: {type: String},
   facebook: {id: String, token: String, photo: String},
+  kakao: {id: String, username:String, displayName:String},
   createdAt: {type: Date, default: Date.now}
 }, {
   toJSON: { virtuals: true},
